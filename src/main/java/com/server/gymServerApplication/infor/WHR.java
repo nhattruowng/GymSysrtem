6ce4edfee081308675f3c865fce238f876307999
nhattruowng <ltn04098@gmail.com>
2025-01-21 17:07:57 +0700

    add whr entity

--- src/main/java/com/server/gymServerApplication/infor/WHR.java
+++ src/main/java/com/server/gymServerApplication/infor/WHR.java
@@ -3,5 +3,6 @@ package com.server.gymServerApplication.infor;
 public enum WHR {
     LOW_RISK_OF_OBESITY,
     MAY_HAVE_RISK_OF_OBESITY,
-    HIGH_RISK_OF_OBESITY
+    HIGH_RISK_OF_OBESITY,
+    UNCONFIRMED
 }
--- src/main/java/com/server/gymServerApplication/utils/BMI.java
+++ src/main/java/com/server/gymServerApplication/utils/BMI.java
@@ -1,5 +1,9 @@
 package com.server.gymServerApplication.utils;
 
+import com.server.gymServerApplication.infor.Gender;
+import com.server.gymServerApplication.infor.WHR;
+import com.server.gymServerApplication.infor.WHtR;
+
 public class BMI {
 
     public static com.server.gymServerApplication.infor.BMI getBMI(float weight, float height) {
@@ -13,5 +17,29 @@ public class BMI {
         } else {
             return com.server.gymServerApplication.infor.BMI.OBESE;
         }
+    }
+
+    public static WHR getWhr(Gender gender, float round1, float round2) {
+        float whr = round1 / round2;
+        return switch (gender) {
+            case MALE -> (whr < 0.8) ? WHR.LOW_RISK_OF_OBESITY :
+                    (whr >= 0.8 && whr <= 0.89) ? WHR.MAY_HAVE_RISK_OF_OBESITY :
+                            WHR.HIGH_RISK_OF_OBESITY;
+
+            case FEMALE -> (whr < 0.9) ? WHR.LOW_RISK_OF_OBESITY :
+                    (whr >= 0.9 && whr <= 0.99) ? WHR.MAY_HAVE_RISK_OF_OBESITY :
+                            WHR.HIGH_RISK_OF_OBESITY;
+
+            default -> WHR.UNCONFIRMED;
+        };
+    }
+
+    public static WHtR getWhtr(float round2, float height) {
+        float whtr = round2 / height;
+        return (whtr < 0.85) ? WHtR.HEALTHY :
+                (whtr >= 0.85 && whtr <= 0.94) ? WHtR.OVERWEIGHT :
+                        (whtr >= 0.95 && whtr <= 1.04) ? WHtR.OBESITY :
+                                (whtr >= 1.05 && whtr <= 1.14) ? WHtR.MORbid_OBESITY : WHtR.UNCONFIRMED
+                ;
     }
 }
